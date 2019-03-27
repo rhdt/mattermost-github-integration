eafb3255a8f1b0ebf02e7c5cc4dccd0ac621ba15
Aditya Konarde <aditya.konarde@gmail.com>
2019-03-27 13:43:57 +0530

    Use environment variables instead

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ                                                                                                                             
 from sys import exit
-USERNAME = "Github"
-ICON_URL = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+USERNAME = environ['BOT_USERNAME']
+ICON_URL = environ['ICON_URL']
 reponames = [x for x in environ['REPO_NAMES'].split(',')]
 hook_urls = [x for x in environ['HOOK_URLS'].split(',')]
 channels = [x for x in environ['CHANNEL_NAMES'].split(',')]
